700ea017b49231c5906978d3b9189e0347278900
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-02-25 10:17:29 +0000

    added readme questions to questions in index.js file

--- index.js
+++ index.js
@@ -2,19 +2,68 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'What is you GitHub username?🐙'
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'What is you email address?📧'
+    },
+    {
+        type: 'input',
+        name: 'Title',
+        message: "What is your Project's name? 📂"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'How would you describe you project?🖋'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license?🔐',
+        Choices: ["MIT", "APACHE 2.O", "GPL", "NONE"]
+    },
+    {
+        type: 'input',
+        name: 'INSTALLATION',
+        message: 'What command should be run to installed dependencies?💾',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run  to run test?💽',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?🗒',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?🗒',
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    
 }
 
 // function to initialize program
 function init() {
-
+    
 }
 
 // function call to initialize program
